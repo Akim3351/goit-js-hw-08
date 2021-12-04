88f856238c770ff65bdad05f4da444a8dff830a6
Akim Grakovskiy <aakim5586@gmail.com>
2021-12-05 00:37:14 +0200

    05.12.2021

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash/throttle';
 
+const refs = {
+    feedBackForm: document.querySelector('.feedback-form'),
+    emailInput: document.querySelector('input[name=email]'),
+    msgArea: document.querySelector('textarea[name=message]'),
+    submitBtn: document.querySelector('button[type=submit]'),
+};
+
+const feedBack = {
+    email: "",
+    msg: "",
+};
+
+refs.feedBackForm.addEventListener('input', throttle(saveFormData, 500));
+refs.feedBackForm.addEventListener('submit', onFormSubmit);
+
+populateForm();
+
+function populateForm() {
+    const savedFeedback = JSON.parse(localStorage.getItem('feedback'));
+    refs.emailInput.value = savedFeedback.email;
+    refs.msgArea.value = savedFeedback.msg;
+
+};
+
+function saveFormData() {
+    feedBack.email = refs.emailInput.value;
+    feedBack.msg = refs.msgArea.value;
+    localStorage.setItem('feedback', JSON.stringify({ email: `${feedBack.email}`, msg: `${feedBack.msg}` }));
+};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.table(feedBack);
+    refs.emailInput.value = "";
+    refs.msgArea.value = "";
+    saveFormData()
+};

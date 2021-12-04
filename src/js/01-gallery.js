0a107a0a1144d997f4cf8842f8d38a9809681fbd
Akim Grakovskiy <aakim5586@gmail.com>
2021-12-04 22:50:32 +0200

    04.12.2021 22-50
    ## 1 & 2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+createGalleryMarkup(galleryItems);
+function createGalleryMarkup(galleryItems) {
+    const markup = galleryItems
+        .map(item =>
+            `
+            <a class="gallery__item"
+                href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    alt="${item.description}"
+                />
+            </a>
+        `
+        )
+        .join(" ");
+
+    gallery.innerHTML = markup;
+};
+
+gallery.addEventListener('click', openModal);
+function openModal(event) {
+
+        event.preventDefault();
+
+        if (event.target.nodeName !== 'IMG') {
+            return
+        }
+
+    const galleryModal = new SimpleLightbox('.gallery a');
+    console.log('открыли!!');
+    galleryModal.on("show.simplelightbox", function () {
+        galleryModal.options.captionDelay = 250;
+        galleryModal.options.captionsData = 'alt';
+    })
+};
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash/throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', setTime);
+player.on('timeupdate', throttle(getTime, 1000));
+
+function getTime()  {
+    player.getCurrentTime().then(function (currentTime) {
+        localStorage.setItem("videoplayer-current-time", `${currentTime}`);
+    });
+};
+
+function setTime() {
+    const fetchedTime = localStorage.getItem("videoplayer-current-time");
+    player.setCurrentTime(fetchedTime);
+};
